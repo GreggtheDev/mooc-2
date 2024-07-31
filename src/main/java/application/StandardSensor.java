8020d322aeff0fce49e7fdf52d5b958464db6813
GreggtheDev <greggthedev@gmail.com>
2024-07-31 19:23:00 -0400

    contructor for standardsensor

--- src/main/java/application/StandardSensor.java
+++ src/main/java/application/StandardSensor.java
@@ -8,3 +8,49 @@ public class StandardSensor implements Sensor {
     public StandardSensor(int value) {
         this.value = value;
     }
+
+    /**
+     * Always returns true since the sensor is always on.
+     */
+    @Override
+    public boolean isOn() {
+        return true;
+    }
+
+    /**
+     * Does nothing since the sensor is always on.
+     */
+    @Override
+    public void setOn() {
+        // Does nothing
+    }
+
+    /**
+     * Does nothing since the sensor is always on.
+     */
+    @Override
+    public void setOff() {
+        // Does nothing
+    }
+
+    /**
+     * Returns the fixed value of the sensor.
+     */
+    @Override
+    public int read() {
+        return this.value;
+    }
+
+    public static void main(String[] args) {
+        StandardSensor ten = new StandardSensor(10);
+        StandardSensor minusFive = new StandardSensor(-5);
+
+        System.out.println(ten.read());
+        System.out.println(minusFive.read());
+
+        System.out.println(ten.isOn());
+        ten.setOff();
+        System.out.println(ten.isOn());
+    }
+}
+
