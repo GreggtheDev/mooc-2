bb1ae08e357c8bad0f0902f758c1455f8aabcce4
GreggtheDev <greggthedev@gmail.com>
2024-07-31 18:58:25 -0400

    contructor for standardsensor

--- src/main/java/application/StandardSensor.java
+++ src/main/java/application/StandardSensor.java
@@ -1,4 +1,10 @@
 package application;
 
-public class StandardSensor {
-}
+// Always returns a fixed value and always on
+public class StandardSensor implements Sensor {
+    private int value;
+
+    // Constructor for StandardSensor
+    public StandardSensor(int value) {
+        this.value = value;
+    }
