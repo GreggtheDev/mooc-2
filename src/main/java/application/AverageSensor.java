ca259479e08d531c1959a7fa5cfff94ffe4477dd
GreggtheDev <greggthedev@gmail.com>
2024-07-31 19:45:26 -0400

    added sensor settings

--- src/main/java/application/AverageSensor.java
+++ src/main/java/application/AverageSensor.java
@@ -16,3 +16,32 @@ public AverageSensor() {
     this.sensors = new ArrayList<>();
     this.readings = new ArrayList<>();
 }
+
+// Adds a sensor to list of sensors
+public void addSensor(Sensor toAdd) {
+    this.sensors.add(toAdd);
+}
+
+// Returns true if all sensors are on
+public boolean isOn() {
+    for (Sensor sensor : sensors) {
+        if (!sensor.isOn()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Turns on all sensors
+public void setOn() {
+    for (Sensor sensor : sensors) {
+        sensor.setOn();
+    }
+}
+
+// Turns off all sensors
+public void setOff() {
+    for (Sensor sensor : sensors) {
+        sensor.setOff();
+    }
+}
