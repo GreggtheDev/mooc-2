5cba252e6ec4b0946deee85b565ae40d026b20c1
GreggtheDev <greggthedev@gmail.com>
2024-07-31 19:39:26 -0400

    imported java.util.Random

--- src/main/java/application/TemperatureSensor.java
+++ src/main/java/application/TemperatureSensor.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Random;
+
 /**
  * TemperatureSensor is a sensor that randomly returns a temperature value
  * between -30 and 30 when it is on.
@@ -11,3 +13,47 @@ public class TemperatureSensor implements Sensor {
     public TemperatureSensor() {
         this.isOn = false;
     }
+
+    /**
+     * Returns true if the sensor is on, false otherwise.
+     */
+    @Override
+    public boolean isOn() {
+        return this.isOn;
+    }
+
+    /**
+     * Turns the sensor on.
+     */
+    @Override
+    public void setOn() {
+        this.isOn = true;
+    }
+
+    /**
+     * Turns the sensor off.
+     */
+    @Override
+    public void setOff() {
+        this.isOn = false;
+    }
+
+    /**
+     * Returns a random temperature between -30 and 30 if the sensor is on.
+     * Throws an IllegalStateException if the sensor is off.
+     */
+    @Override
+    public int read() {
+        if (!this.isOn) {
+            throw new IllegalStateException("The sensor is off");
+        }
+        Random random = new Random();
+        return random.nextInt(61) - 30; // Random number between -30 and 30
+    }
+
+    public static void main(String[] args) {
+        Sensor kumpula = new TemperatureSensor();
+        kumpula.setOn();
+        System.out.println("temperature in Kumpula " + kumpula.read() + " degrees Celsius");
+    }
+}
